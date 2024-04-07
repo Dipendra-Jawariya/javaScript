12c5a1a64f4305d4ca28ee0de467b2e701dad342
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2024-04-07 19:14:28 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accoundId = 1445;
+let accountEmail = "dipendrajawariya232gmail.com";
+var accountPswd = "123456";
+accountCity = "Jaipur";
+
+
+let accountState; // shows undefined;
+
+// accoundId = 2;
+/*
+prefer not to use var because of issue in block scope and functional scope
+
+*/
+accountEmail = "d@.com"
+accountPswd = "2334678"
+accountCity = "Bengaluru"
+console.log(accoundId);
+console.table([accoundId,accountEmail,accountPswd,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treats all js code as newer version
+// alert(3 + 3) // we are using node js not browser
+
+let name = "Dipendra" // string datatype
+let age = 22 // int datatype
+let isloggedIn = true // boolean datatype
+let state; // undefined
+// number 2 to power 53
+// bigInt
+// string => ""  or '' bu more preferale is ""
+//  boolean => true /false
+//  null  ==> standalone type h
+// undefined => value not asigned
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof "dipendra");
+console.log(typeof undefined); // shows undefined
+console.log(typeof null); // shows object
